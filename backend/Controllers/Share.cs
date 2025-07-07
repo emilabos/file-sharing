76eef42512bc99b2e0c5639d0b71be64317861f5
emilabos <138469562+emilabos@users.noreply.github.com>
2025-07-07 21:29:52 +0100

    dsfsdfsdfdsfshhhshfhsfsf

---
+++ backend/Controllers/Share.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Share : ControllerBase
+    {
+        Random R = new Random();
+        private static readonly List<dataModel> datas = new();
+        private static int nextId = 1;
+
+        [HttpPost]
+        public IActionResult Add([FromBody] dataModel data)
+        {
+            data.Id = nextId++;
+            data.Code = generateCode();
+            datas.Add(data);
+            return Ok(data.Code);
+        }
+
+        [HttpGet("{code}")]
+        public IActionResult GetByCode(int code)
+        {
+            dataModel data = datas.FirstOrDefault(x => x.Code == code);
+            if (data == null)
+                return NotFound();
+
+            return Ok(data);
+        }
+
+        public int GenerateCode()
+        {
+            int code = 0;
+            bool run = true;
+            int[] nums = datas.Select(x => x.Code).ToArray();
+            while (run)
+            {
+                run = false;
+                code = R.Next(100000, 999999);
+                Array.ForEach(nums, x =>
+                {
+                    if (x == code) run = true;
+                });
+            }
+            return code;
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,23 +1,25 @@
-﻿
-using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Collections;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace AESEncryption
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-        byte[] key = Encoding.ASCII.GetBytes("Thats my Kung Fu"); 
-        byte[] plaintext = Encoding.ASCII.GetBytes("Two One Nine Two");
-
-        byte[] encrypted = AES.Encrypt(plaintext, key);
-        byte[] decrypted = AES.Decrypt(encrypted, key);
-
-        Console.WriteLine("Encrypted: " + BitConverter.ToString(encrypted));
-        Console.WriteLine("Decrypted: " + Encoding.ASCII.GetString(decrypted));
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
