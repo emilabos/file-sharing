4d8d34d25f9729155b2b968554f1dcbc99ef612a
PeterAbos <abos.petike@gmail.com>
2025-07-07 20:38:03 +0100

    back back

---
+++ backend1/backend/Models/dataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class dataModel
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Data { get; set; }
+    }
+}
